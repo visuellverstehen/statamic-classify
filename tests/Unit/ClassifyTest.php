8e3632a32be425f1e0e6f3b0ad89b692dfd48c84
Flemming Petersen <mail@flemming-petersen.de>
2020-08-19 15:14:59 +0200

    [TASK] Add a basic test for the classify class

--- tests/Unit/ClassifyTest.php
+++ tests/Unit/ClassifyTest.php
@@ -2,24 +2,45 @@
 
 namespace VV\Classify\Tests\Unit;
 
+use Illuminate\Support\Facades\Config;
+use VV\Classify\Modifiers\Classify;
 use VV\Classify\Tests\TestCase;
 
 class ClassifyTest extends TestCase
 {
+    /**
+     * @var Classify
+     */
+    protected $classify;
+
     public function setUp(): void
     {
         parent::setUp();
 
         $config = [
-            //
+            'default'  => [
+                'h1' => 'headline',
+            ],
         ];
 
-        //
+        Config::set('classify', $config);
+
+        $this->classify = new Classify();
+
+
     }
 
     /** @test */
     public function test()
     {
-        $this->assertTrue(true);
+        $value = '<h1> Hello world</h1>';
+        $params = [];
+        $context = [];
+
+        $classified = $this->classify->index($value, $params, $context);
+
+        $expectedClassifiedString = '<h1 class="headline"> Hello world</h1>';
+
+        $this->assertEquals($classified, $expectedClassifiedString);
     }
 }
